1cb67ca83839a42a86c74decdad6bc2c7af7bd39
Shinichi <miyakawashi@gmail.com>
2022-03-27 18:06:28 -0400

    contact form handling

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from 'react-router-dom';
 
 export const ContactForm = () => {
+   const [email, setEmail] = useState('');
+   const [message, setMessage] = useState('');
+   const [name, setName] = useState('');
+
+   const handleMessageChange = (event) => {
+      setMessage(event.target.value);
+   };
+
+   const handleEmailChange = (event) => {
+      setEmail(event.target.value);
+   };
+
+   const handleNameChange = (event) => {
+      setName(event.target.value);
+   };
+
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log('submitted');
+      setMessage('');
+      setName('');
+      setEmail('');
+   }
    return (
         <div style={{width: '95%'}} className="background-dark">
-           <form style={{}} className="flex-column">
+           <form style={{}} className="flex-column" onSubmit= {handleSubmit}>
                <label htmlFor="name">Name: </label>
-               <input name="name" placeholder=""></input>
+               <input name="name" placeholder="" value={name} onChange={handleNameChange}></input>
                <label htmlFor="email">Email: </label>
-               <input name="email" placeholder=""></input>
+               <input name="email" type="email" value={email} onChange={handleEmailChange}></input>
                <label htmlFor="message">Message:</label>
-               <textarea name="message" placeholder="Send me a message!">
+               <textarea name="message" placeholder="Send me a message!" value={message} onChange={handleMessageChange}>
                </textarea>
-               <button style={{width: '15%'}} className="mt-3" type='button'>Submit</button>
+               <button style={{width: '15%'}} className="mt-3" type='submit'>Submit</button>
            </form>
         </div>
    ) 
