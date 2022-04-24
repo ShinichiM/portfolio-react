9978225ffcf51e7d2b368bb86fb1fd47e0943aad
Shinichi <miyakawashi@gmail.com>
2022-04-24 19:34:45 -0400

    removed unneeded content

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div style={{minHeight: '100vh'}} className="page-container">
-      ADD FILLER OR ELSE!
+      {/* ADD FILLER OR ELSE! */}
       {/* <div style={{width: '100vw', height: '100vh'}} className="img-background"></div> */}
     </div>
   );
