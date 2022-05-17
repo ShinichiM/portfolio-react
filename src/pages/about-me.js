bc6fbbc47c2aa014cd69cf206dafc6ffa535bd0f
Shinichi <miyakawashi@gmail.com>
2022-05-16 21:07:14 -0400

    description change

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div style={{ overflowY: "scroll" }} className="h-100-vh page-container background-beige border-dark">
+    <div
+      style={{ overflowY: "scroll" }}
+      className="h-100-vh page-container background-beige border-dark"
+    >
       <div className="tag">
         <div className="about-me-img-container">
-          <div  className="p-2 about-me-content-container">
-{/* background-dark */}
-            <div
-              style={{ display: "flex" }}
-              className=" align-center"
-            >
+          <div className="p-2 about-me-content-container">
+            {/* background-dark */}
+            <div style={{ display: "flex" }} className=" align-center">
               <div className="selfie-container">
                 <img
                   style={{ width: "100%", height: "100%" }}
@@ -31,17 +31,17 @@ const AboutMe = () => {
           <div className="p-2 about-me-content-container ">
             <h1 className="text-format ml-2">About Me</h1>
             <p className="ml-2">
-              Front-end software engineer leveraging a Computer Systems
-              Validation and Applied Physics background to create a more performant and accessible
-              user experience on the web. I am a recent graduate of The Ohio
-              State University's full-stack web development bootcamp where I
-              created responsive web applications utilizing React.js,
+              Junior software engineer leveraging a Computer Systems Validation
+              and Applied Physics background to create a more performant and
+              accessible user experience on the web. I am a recent graduate of
+              The Ohio State University's full-stack web development bootcamp
+              where I created responsive web applications utilizing React.js,
               JavaScript, Node.js, Express.js, and several CSS frameworks. I
               work with computer validation and as a result, I excel at making
               sure applications function properly. I use these skills as well as
-              my creativity to create sites that are simple, intuitive, and
-              responsive. I am excited to contribute my skills and experience to
-              a team dedicated to developing user focused applications.
+              my creativity to create sites that are simple and intuitive. I am
+              looking to contribute my skills and experience to a team dedicated
+              to developing user focused applications.
             </p>
           </div>
         </div>
